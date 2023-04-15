b30b2bff28f784d27b27cb7350d17cab6fa5db04
Gustavo <gusguims@outlook.com>
2023-04-15 18:36:08 -0300

    def main created

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from selenium import webdriver
 import os
 import pandas as pd
 
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-from time import sleep
 
 # Inicializando o serviço do Chrome
 service = Service('/home/gustgmrs/Downloads/chromedriver')
@@ -44,12 +42,10 @@ def login(name, passwd):
     # Preenchendo nome
     user = driver.find_element(By.XPATH, '/html/body/div/form/input[1]')
     user.send_keys(name)
-    sleep(0.5)
 
     # Preenchendo senha
     password = driver.find_element(By.XPATH, '/html/body/div/form/input[2]')
     password.send_keys(passwd)
-    sleep(0.5)
 
     # Clicando no botão
     button = driver.find_element(By.XPATH, '/html/body/div/form/button')
@@ -57,11 +53,73 @@ def login(name, passwd):
 
 
 def main():
+    '''
+    Função principal que iterará sobre o dataframe e preencherá os campos
+
+    Parâmetros:
+        None
+    Returns:
+        None
+    '''
+
     # Logando no sistema
     login('gustavo', '123')
 
     # Criando dataframe
-    df_notas = create_dataframe('files/NotasEmitir.xlsx')
+    df_notes = create_dataframe('files/NotasEmitir.xlsx')
+
+    for line in df_notes.index:
+        # Coletando e preenchendo dados
+        # Preenchendo nome
+        name = driver.find_element(By.NAME, 'nome')
+        name.send_keys(df_notes.loc[line, 'Cliente'])
+
+        # Preenchendo endereço
+        endereco = driver.find_element(By.NAME, 'endereco')
+        endereco.send_keys(df_notes.loc[line, 'Endereço'])
+
+        # Preenchendo bairro
+        bairro = driver.find_element(By.NAME, 'bairro')
+        bairro.send_keys(df_notes.loc[line, 'Bairro'])
+
+        # Preenchendo município
+        municipio = driver.find_element(By.NAME, 'municipio')
+        municipio.send_keys(df_notes.loc[line, 'Municipio'])
+
+        # Preenchendo CEP
+        cep = driver.find_element(By.NAME, 'cep')
+        cep.send_keys(str(df_notes.loc[line, 'CEP']))
+
+        # Preenchendo UF
+        uf = Select(driver.find_element(By.NAME, 'uf'))
+        uf.select_by_visible_text(df_notes.loc[line, 'UF'])
+
+        # Preenchendo CPF/CNPJ
+        cpf_cnpj = driver.find_element(By.NAME, 'cnpj')
+        cpf_cnpj.send_keys(str(df_notes.loc[line, 'CPF/CNPJ']))
+
+        # Preenchendo inscrição estadual
+        descricao = driver.find_element(By.NAME, 'inscricao')
+        descricao.send_keys(str(df_notes.loc[line, 'Inscricao Estadual']))
+
+        # Preenchendo descrição
+        descricao = driver.find_element(By.NAME, 'descricao')
+        descricao.send_keys(df_notes.loc[line, 'Descrição'])
+
+        # Preenchendo quantidade
+        quantidade = driver.find_element(By.NAME, 'quantidade')
+        quantidade.send_keys(str(df_notes.loc[line, 'Quantidade']))
+
+        # Preenchendo valor unitário
+        valor_unitario = driver.find_element(By.NAME, 'valor_unitario')
+        valor_unitario.send_keys(str(df_notes.loc[line, 'Valor Unitario']))
+
+        # Preenchendo valor total
+        valor_total = driver.find_element(By.NAME, 'total')
+        valor_total.send_keys(str(df_notes.loc[line, 'Valor Total']))
+
+        # Recarregando página para limpar os campos
+        driver.refresh()
 
 
 if __name__ == '__main__':
